0b2d303641125d434e9dc8fa8f1befa5d2c1f2b3
Drew Welch <dwelch@redhat.com>
2025-04-14 15:47:50 -0400

    add base for er_aws_cloudwatch module to process inputs

---
+++ er_aws_cloudwatch/app_interface_input.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+from external_resources_io.input import AppInterfaceProvision
+from pydantic import BaseModel
+
+
+class CloudwatchData(BaseModel):
+    """Data model for AWS Cloudwatch"""
+
+    # app-interface
+    region: str
+    identifier: str
+    output_resource_name: str | None = None
+    es_identifier: str | None = None
+    release_url: str | None = None
+    tags: dict[str, Any] | None = None
+
+    # aws_cloudwatch_log_group
+    retention_in_days: int
+
+    # aws_lambda_function
+    handler: str | None = None
+    memory_size: int | None = None
+    runtime: str | None = None
+    timeout: int | None = None
+
+    # aws_cloudwatch_log_subscription_filter
+    filter_pattern: str | None = None
+
+
+class AppInterfaceInput(BaseModel):
+    """Input model for AWS Cloudwatch"""
+
+    data: CloudwatchData
+    provision: AppInterfaceProvision
